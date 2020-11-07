c2c6297748c7c6a816a5a22157d97e04d2f1c425
Mick <grevmivlos@gmail.com>
2020-11-07 15:34:28 +0100

    removed last week from this repo

--- F3W1D2/day1/src/file1.js
+++
@@ -1,7 +0,0 @@
-export const text1 = "Hello";
-export const text2 = "Hello, World!";
-export const text3 = "Hello wonderful world";
-
-export default function (str) {
-  return str.toUpperCase();
-}
